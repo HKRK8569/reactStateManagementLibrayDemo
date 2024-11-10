447881b00004acc8e44becdfcb40cee3a4a1a9dc
HKRK8569 <kayann3021@gmail.com>
2024-11-10 21:40:02 +0900

    tailwindCssを設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "sanitize.css";
 import AppRoutes from "./routes/AppRoutes.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
