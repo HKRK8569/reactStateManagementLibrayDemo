b04db6365dc7f059a404af0cd60f10db59596d0f
HKRK8569 <kayann3021@gmail.com>
2024-11-16 01:03:53 +0900

    Recoilページの設定

---
+++ src/pages/Recoil/index.tsx
@@ -0,0 +1,16 @@
+import recoilLogo from "@/assets/recoil.svg";
+
+const Recoil = () => {
+  return (
+    <div>
+      <div className="max-w-screen-md mx-auto">
+        <header className="mt-20 flex flex-col items-center">
+          <img src={recoilLogo} className="h-20" alt="react logo" />
+        </header>
+        <div className="mt-10"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Recoil;
--- src/pages/Top/components/LogoPageLinks/index.tsx
+++ src/pages/Top/components/LogoPageLinks/index.tsx
@@ -24,7 +24,7 @@ const links = [
   {
     img: recoilLogo,
     alt: "recoil logo",
-    href: "/",
+    href: PAGES.RECOIL,
   },
 ];
 
